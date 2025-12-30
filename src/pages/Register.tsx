83491a0f22e307fa0e19639419ddbef295f74d26
brandontautuan <124856386+brandontautuan@users.noreply.github.com>
2025-12-29 18:05:04 -0800

    Form edits minor UI

--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -5,7 +5,7 @@ function Register() {
         <h1 className="page-title">Register</h1>
         <div className="section-content">
           <p className="section-text">
-            Registration for the Los Rios STEM Fair is coming soon! For now, you can register your interest here: <a href="https://forms.gle/1234567890">Interest Form</a>.
+            Registration for the Los Rios STEM Fair is coming soon! For now, you can register your interest here: <a href="https://forms.gle/evzMxp7bMHRrw8eJA" target="_blank" rel="noopener noreferrer">Interest Form</a>.
           </p>
         </div>
       </section>
